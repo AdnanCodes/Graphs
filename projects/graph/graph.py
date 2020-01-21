d6d78ca21b265da8158603d9cee517a9c59ba237
Adnan <adnan.chowdhury@outlook.com>
2020-01-21 11:13:54 -0600

    add edge and neighbors done

--- projects/graph/graph.py
+++ projects/graph/graph.py
@@ -21,14 +21,21 @@ class Graph:
     def add_edge(self, v1, v2):
         """
         Add a directed edge to the graph.
+
+        If both exist, and a connection from v1 to v2
         """
         # TODO
+        if v1 in self.vertices and v2 in self.vertices:
+            self.vertices[v1].add(v2)
+        else:
+            raise IndexError("That vertex does not exist!")
 
     def get_neighbors(self, vertex_id):
         """
         Get all neighbors (edges) of a vertex.
         """
-        pass  # TODO
+        # TODO
+        return self.vertices[vertex_id]
 
     def bft(self, starting_vertex):
         """
